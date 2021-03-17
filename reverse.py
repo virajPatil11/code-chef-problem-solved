1df9647a7a22c0c4cef7405e38f3836d14703277
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-17 09:31:44 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 17:45:34 2021
+
+@author: raj patil
+"""
+
+t = int(input("E"))
+for i in range(t):
+    no = input()
+    nu=no[::-1]
+    nu=int(nu)
+    print(nu)
+
+    
+    
