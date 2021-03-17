aba3fa39e36ae7f7513dd1aeee18e4c9cf01201d
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-17 09:50:53 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 09:39:31 2021
+
+@author: raj patil
+"""
+t = int(input())
+for i in range(t):
+    
+    number = int(input("enter a number: "))
+    sqrt = number ** 0.5
+    print(int(sqrt))
