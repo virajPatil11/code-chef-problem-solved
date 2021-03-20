53b2c27befe9a5f8c6150abbf1bc42b3cbfb1585
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-20 09:03:27 +0530

    Add files via upload

---
+++ cupCake.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 08:46:59 2021
+
+@author: raj patil
+"""
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = 2
+    if n == 2:
+        print(2)
+    else:
+        print(n // a + 1)
+    
+
+    
