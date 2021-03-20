ddf40a687db8896fbe739c318b4b6a875a4e96a6
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-20 09:33:47 +0530

    Add files via upload

---
+++ helpingChef.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 09:26:43 2021
+
+@author: raj patil
+"""
+for i in range(int(input())):
+    num = int(input())
+    if num <= 10:
+        print("Thanks for helping Chef!")
+    else:
+        print("-1")
+        
---
+++ smallFactorial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 09:11:51 2021
+
+@author: raj patil
+"""
+
+from math import*
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(factoria(n)
