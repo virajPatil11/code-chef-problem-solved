cc5f7ea413fbc9c4e44a89d8e8f095edd1d374a6
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-24 09:45:58 +0530

    Add files via upload

---
+++ sumOrDiffernce.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 09:45:13 2021
+
+@author: raj patil
+"""
+
+a = int(input())
+b = int(input())
+if a > b:
+    print(a - b)
+else:
+    print(a + b)
